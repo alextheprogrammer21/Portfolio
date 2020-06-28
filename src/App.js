7af034e034a4012f3c68fc534946931d64c06b24
Alex S <alextheprogrammer21@gmail.com>
2020-06-27 23:18:27 -0700

    Added props to cards

--- src/App.js
+++ src/App.js
@@ -7,24 +7,36 @@ import Navigation from './Components/Navigation';
 import Card from './Components/Card'
 import './App.css';
 
-
+const projects = [
+  {id: 1, name: 'Devware', url: 'https://alextheprogrammer21.github.io/Devware/', sourceCode: 'https://github.com/alextheprogrammer21/Devware', description: 'People make software to sell to you. People make freeware to give away to you. There is also a vast sea of open source projects hidden to the average user that developers make for various reasons. I call these devwares. Devware allows users to search for and download devware applications for free.', img: "https://raw.githubusercontent.com/alextheprogrammer21/Re-Mind/master/images/1.PNG"}
+]
 function App() {
+
+  const listOfProjects = projects.map((project) => {
+    return (
+      <Card
+        name={project.name}
+        url={project.url}
+        sourceCode={project.sourceCode}
+        img={project.img}
+        description={project.description}
+      />
+    );
+  });
+
   return (
     <div className="App">
             <Navigation></Navigation>
       <header className="App-header">
         <p className='cards'>
-          <Card></Card> <Card></Card>
+          { listOfProjects }
         </p>
       </header>
-
       <footer>
   <p>Made by Abhi Setia </p>
   <p>Contact: <a href="mailto:abhisetia0@gmail.com.com">abhisetia0@gmail.com</a></p>
 </footer>
-
     </div>
-    
   );
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import "./Card.css";
 
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className='Container'> 
             <img src="https://raw.githubusercontent.com/alextheprogrammer21/Re-Mind/master/images/1.PNG" />
             <div className='Project'>
                 <header>
-                    <h1>Devware</h1>
+                    <h1>{props.name}</h1>
                 </header>
-                <p>People make software to sell to you. People make freeware to give away to you. There is also a vast sea of open source projects hidden to the average user that developers make for various reasons. I call these devwares.
-                Devware allows users to search for and download devware applications for free.
-                </p>
-                <h6><a href="url">Visit Site</a></h6>
-                <h6><a href="url">Source Code</a></h6>
+                <p> {props.description} </p>
+                <h6><a href={props.url}>Visit Site</a></h6>
+                <h6><a href={props.sourceCode}>Source Code</a></h6>
             </div>
         </div>
     )
