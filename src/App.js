ba008e2fc656134ef36f498e16c4282dcc5f8cd1
Alex S <alextheprogrammer21@gmail.com>
2020-06-29 15:49:01 -0700

    Added a website to portfolio

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Contact from './Components/Contact';
 import './App.css';
 
 const projects = [
+  {id: 3, name: 'Interview Scheduler', url: 'https://alextheprogrammer21.github.io/Interview-Scheduler/', sourceCode: 'https://github.com/alextheprogrammer21/Interview-Scheduler', description: 'A scheduler that allows users to set up, edit, or delete appointments.', img: "https://raw.githubusercontent.com/alextheprogrammer21/Interview-Scheduler/master/public/images/Screenshot0.png"},
   {id: 1, name: 'Devware', url: 'https://alextheprogrammer21.github.io/Devware/', sourceCode: 'https://github.com/alextheprogrammer21/Devware', description: 'People make software to sell to you. People make freeware to give away to you. There is also a vast sea of open source projects hidden to the average user that developers make for various reasons. I call these devwares. Devware allows users to search for and download devware applications for free.', img: "https://raw.githubusercontent.com/alextheprogrammer21/Devware/master/src/images/1.PNG"},
   {id: 2, name: 'Re:Mind', url: 'https://alextheprogrammer21.github.io/Re-Mind/', sourceCode: 'https://github.com/alextheprogrammer21/Re-Mind', description: 'A wellness app for habit tracking.', img: "https://raw.githubusercontent.com/alextheprogrammer21/Re-Mind/master/images/1.PNG"}
-
 ]
 
 function App() {
