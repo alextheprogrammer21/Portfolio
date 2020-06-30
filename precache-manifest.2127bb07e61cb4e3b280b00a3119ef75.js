6d18a87edf3471b54b127ea32c2e063d5687382c
Alex S <alextheprogrammer21@gmail.com>
2020-06-29 19:18:30 -0700

    Updates

---
+++ precache-manifest.2127bb07e61cb4e3b280b00a3119ef75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f2ebbd579d26d9aed713ee32a2d6d3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76179dd64c5724ab65e2",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b588e15fbefc113adae9",
+    "url": "/portfolio/static/css/main.690bfcba.chunk.css"
+  },
+  {
+    "revision": "76179dd64c5724ab65e2",
+    "url": "/portfolio/static/js/2.d92923bc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.d92923bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b588e15fbefc113adae9",
+    "url": "/portfolio/static/js/main.53550f93.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
