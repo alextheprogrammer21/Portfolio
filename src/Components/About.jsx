dafe64896c0b4677f87aac077099d4904459d5ab
Alex S <alextheprogrammer21@gmail.com>
2020-06-29 19:24:53 -0700

    Modified about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
         <br></br>
         <p>Right now I'm sure you're wondering if you've found someone that's able to quickly integrate into your workspace and work well with the team.</p>
         <br></br>
-        <p>I've worked at startups and SMEs in my previous career for a little over two years and have been able to contribute and exceed in collaborative environments. 
+        <p>I've worked at startups and SMEs in my previous career for a little over two years and have been able to quickly integrate and contribute to corporate goals immediately. 
             Furthermore, I've spent three months, 12 hours per day learning full-stack development at Lighthouse Labs bootcamp. During the bootcamp, I learned to become proficient in Javascript, Ruby, HTML, CSS, ReactJS, NodeJS, jQuery, 
             Ajax, Sass, PostgreSQL, Mocha and Chai, Storybook, and linux. The only reason this was possible was because we spent the three months building real-world applications 
             with mentorship support. We were expected to write clean code and work in teams, as well as individually in order to simulate a real world working environment.
@@ -23,7 +23,7 @@ export default function About() {
         <p> I believe by going through this experience and learning to quickly integrate into different environments and produce results immediately, I am able to save hiring costs
              for companies seeking a developer. </p>
              <br></br>
-        <p>You can find my github with my complete projects <a href="https://www.github.com/alextheprogrammer21" target="_blank">here</a></p>
+        <p>You can find my github with my completed projects <a href="https://www.github.com/alextheprogrammer21" target="_blank">here</a></p>
 
         </div>
     )
