d3c6370a5d90cd3623aee058ee2412806e9c25fd
Alex S <alextheprogrammer21@gmail.com>
2020-06-29 19:08:16 -0700

    Completed the site

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,6 +4,25 @@ import "./About.css";
 
 export default function About() {
     return (
-        <p> About test </p>
+        <div className='about'>
+
+        <h1>Welcome! I'm Abhi Setia</h1>
+        <br></br>
+        <h3>My mission is to help startup and SMEs with web services which are robust, sustainable, and ready to scale.</h3>
+        <br></br>
+        <p>Right now I'm sure you're wondering if you've found someone that's able to quickly integrate into your workspace and work well with the team.</p>
+        <br></br>
+        <p>I've worked at small-team startups and SMEs in my previous career for a little over two years now and have been able to contribute and exceed in collaborative environments. 
+            Furthermore, I've spent three months, 12 hours per day learning full-stack development at Lighthouse Labs. During the bootcamp, I learned to become proficient in Javascript, Ruby, HTML, CSS, ReactJS, NodeJS, jQuery, 
+            Ajax, Sass, PostgreSQL, Mocha and Chai, Storybook, and linux. The only reason this was possible was because we spent the three months building real-world applications 
+            with mentorship support. We were expected to write clean code and work in teams, as well as individually in order to simulate a real world working environment.
+        </p>
+        <br></br>
+        <p> I believe by going through this experience and learning to quickly integrate into different environments and produce results immediately, I am able to save hiring costs
+             for companies seeking a developer. </p>
+             <br></br>
+        <p>You can find my github with my complete projects <a href="https://www.github.com/alextheprogrammer21" target="_blank">here</a></p>
+
+        </div>
     )
 }
