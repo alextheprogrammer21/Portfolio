6f01751a71952eeb35df20d2ee941a99f9319e74
Alex S <alextheprogrammer21@gmail.com>
2020-06-29 19:11:50 -0700

    Added an image to the about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,14 +6,16 @@ export default function About() {
     return (
         <div className='about'>
 
+        <img src='https://avatars3.githubusercontent.com/u/56317276?s=400&u=c560973e56f66a6a2ba9c49aa6e3cd6f904181a7&v=4' />
+
         <h1>Welcome! I'm Abhi Setia</h1>
         <br></br>
         <h3>My mission is to help startup and SMEs with web services which are robust, sustainable, and ready to scale.</h3>
         <br></br>
         <p>Right now I'm sure you're wondering if you've found someone that's able to quickly integrate into your workspace and work well with the team.</p>
         <br></br>
-        <p>I've worked at small-team startups and SMEs in my previous career for a little over two years now and have been able to contribute and exceed in collaborative environments. 
-            Furthermore, I've spent three months, 12 hours per day learning full-stack development at Lighthouse Labs. During the bootcamp, I learned to become proficient in Javascript, Ruby, HTML, CSS, ReactJS, NodeJS, jQuery, 
+        <p>I've worked at startups and SMEs in my previous career for a little over two years and have been able to contribute and exceed in collaborative environments. 
+            Furthermore, I've spent three months, 12 hours per day learning full-stack development at Lighthouse Labs bootcamp. During the bootcamp, I learned to become proficient in Javascript, Ruby, HTML, CSS, ReactJS, NodeJS, jQuery, 
             Ajax, Sass, PostgreSQL, Mocha and Chai, Storybook, and linux. The only reason this was possible was because we spent the three months building real-world applications 
             with mentorship support. We were expected to write clean code and work in teams, as well as individually in order to simulate a real world working environment.
         </p>
